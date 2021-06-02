5976bacf3158dc5f0ab854314e3b53986cefa45e
2x11 <lune2x11epee@gmail.com>
2021-06-01 22:29:24 -0300

    get email

--- user.js
+++ user.js
@@ -1,4 +1,16 @@
 const app = document.querySelector('.container')
-const getUrl = new URLSearchParams(window.location.search)
-id = getUrl.get('id')
-console.log(id)
+const getUrl = new URLSearchParams(window.location.search) //desglosando URL
+id = getUrl.get('id') //capturando id
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+fetch(`${url}/${id}`)
+.then(res => res.json())
+.then(data => {
+    const name = document.createElement('p')
+    name.innerHTML = data.name
+    const email = document.createElement('p')
+    email.innerHTML = data.email
+    app.appendChild(name)
+    app.appendChild(email)
+})
+.catch(err => console.log(err))
